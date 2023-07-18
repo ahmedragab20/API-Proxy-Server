5aabef7f8c961c4565bd8439813791b0612f64e1
ahmedragab <ahmedragab20901@gmail.com>
2023-07-18 09:27:35 +0300

    💚 Add Vue.js

---
+++ app/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-slate-200 p-20">
+    <div class="text-3xl font-bold">Hello Vite + Vue 3 + TypeScript + Tailwind CSS!</div>
+  </div>
+</template>
